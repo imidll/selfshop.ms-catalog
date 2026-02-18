c39239f1e3e4ed6ef0f168e2e349a1b88db2a955
imidll <imi.dll@mail.ru>
2026-02-18 17:12:18 +0400

    feat: add predefined constants for environment and log levels
    Магические строки в коде были подвержены опечаткам и затрудняли рефакторинг.

---
+++ internal/platform/config/enums.go
@@ -0,0 +1,19 @@
+package config
+
+const (
+	AppRunmodeDev  = "dev"
+	AppRunmodeProd = "prod"
+)
+
+const (
+	LogMinLevelDebug = "debug"
+	LogMinLevelInfo  = "info"
+	LogMinLevelWarn  = "warn"
+	LogMinLevelError = "error"
+	LogMinLevelPanic = "panic"
+	LogMinLevelFatal = "fatal"
+
+	LogFormatAuto    = "auto"
+	LogFormatJSON    = "json"
+	LogFormatConsole = "console"
+)
